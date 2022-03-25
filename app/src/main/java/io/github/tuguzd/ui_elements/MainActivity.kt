8a2c45515106a4eaad1402479299633b92c9778b
Damir Tugushev <0damir.1tugushev@gmail.com>
2022-03-25 17:29:18 +0300

    Initial commit

---
+++ app/src/main/java/io/github/tuguzd/ui_elements/MainActivity.kt
@@ -0,0 +1,41 @@
+package io.github.tuguzd.ui_elements
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.activity.viewModels
+import io.github.tuguzd.ui_elements.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private val viewModel: MainActivityModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.run {
+            viewModel.text.observe(this@MainActivity) {
+                textView.text = it
+            }
+
+            button.setOnClickListener {
+                val editText = editText.text
+                val checkBox = if (checkBox.isChecked) "ON" else "OFF"
+                val toggleButton = if (toggleButton.isChecked) "ON" else "OFF"
+                val radioGroup = when {
+                    radioButton1.isChecked -> radioButton1.text
+                    radioButton2.isChecked -> radioButton2.text
+                    else -> "OFF"
+                }
+                val text = """
+                    EditText: $editText
+                    CheckBox: $checkBox
+                    ToggleButton: $toggleButton
+                    RadioGroup: $radioGroup
+                """.trimIndent()
+                viewModel.setText(text)
+            }
+        }
+    }
+}
